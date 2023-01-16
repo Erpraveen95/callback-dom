0d1890860a8b759d8eff3c9197525ae69e6fb6ab
Urs-Praveen <112169678+Urs-Praveen@users.noreply.github.com>
2023-01-16 22:30:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function attachEventListner(){
+    let count=0
+    document.getElementById("10").addEventListener("click",function xyz(){
+        console.log("hit",++count)
+    })
+}
+attachEventListner();
+addEventListener(onload,function(){
+    console.log("dom contents loaded")
+})
+
+window.addEventListener('load', function () {
+    console.log("DOM has loaded")
+  })
